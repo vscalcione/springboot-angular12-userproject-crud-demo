087a047bd7bc85b3e35a02b33f21a1ed4d99164e
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-15 20:19:09 +0200

    Update project

--- userproject-frontend-crud-demo/src/app/app.module.ts
+++ userproject-frontend-crud-demo/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
+import { UpdateUserComponent } from './components/users/update-user/update-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserDetailsComponent,
+    AddUserComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
